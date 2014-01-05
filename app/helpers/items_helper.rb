c5a1eaeeb8f1973bd5c08f2da2e1a0bf0efa7ba9
Chris Slamar <chris@slamar.com>
2014-01-04 18:00:18 -0600

    basics with unordered service

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+	def complete_label(completed)
+		if completed
+			return '<span class="label label-success">Completed</span>'
+		else
+			return '<span class="label label-warning">In Progress</span>'
+		end
+	end
 end
