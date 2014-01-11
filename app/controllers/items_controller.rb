20936262b9eb94d83c82c6535dc2875e6be27be7
Chris Slamar <chris@slamar.com>
2014-01-11 12:46:26 -0600

    basics for completion through modal

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,23 @@ class ItemsController < ApplicationController
   end
 
   def complete
-  	respond_to do |format|
-  		format.json { render json: {'something' => 'asdf'}, status: :ok }
-  	end
+  	wid = params[:wid]
+  	sid = params[:sid]
+
+  	active_widget = Widget.find(wid)
+  	active_service = active_widget.services.detect {|s| s["_id"].to_s == sid }
+
+  	# puts "Short Desc: #{active_service.short_description}"
+
+  	active_service.completed = true
+  	active_service.finish = params[:complete_date]
+
+  	if active_service.save
+	  	respond_to do |format|
+	  		format.json { render json: {'something' => 'asdf'}, status: :ok }
+	  	end
+	  end
+
   end
 
 end
