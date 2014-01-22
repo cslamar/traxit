e13a5bfcc1416b6ffe14e6ed12c094371cf178f2
Chris Slamar <chris@slamar.com>
2014-01-22 15:19:43 -0600

    Map/Reduce helper function and starting point for properties list

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -11,4 +11,30 @@ module ItemsHelper
     return /(\d)-thumb.jpg/.match(image).to_a[1].to_i
   end
 
+  def properties_map_reducer
+    map = %q{
+      function() {
+        for (var key in this.properties) {
+          emit(key, null);
+        }
+      }
+    }
+
+    reduce = %q{
+      function(key, stuff) {
+        return null;
+      }
+    }
+
+    results = Widget.map_reduce(map, reduce).out(inline: true)
+
+    property_list = Array.new
+
+    results.each do |r|
+      property_list.push(r["_id"])
+    end
+
+    return property_list
+  end
+
 end
