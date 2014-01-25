5b389edc39fc471421ff72978b1f12f0140c557a
Chris Slamar <chris@slamar.com>
2014-01-25 14:53:02 -0600

    Added links to service manual on the item info page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,20 @@ module ApplicationHelper
     end
   end
 
-  def check_service_manual
+  def regex_simple_name(simple_name)
+    regex_simple_name = simple_name.gsub(/\'/, '')
+    regex_simple_name = regex_simple_name.gsub(/\s/, '_')
 
+    return regex_simple_name
   end
+
+  def check_service_manual(wid,simple_name)
+
+    if File.exist?("public/media/#{wid}/#{regex_simple_name(simple_name)}.pdf")
+      return true
+    else
+      return false
+    end
+  end
+
 end
